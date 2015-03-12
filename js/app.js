181cc82de6cee4500f3dbba5a7afe6e66e12538d
gongdashi <gongdashi@hotmail.com>
2015-03-12 15:36:57 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,18 @@
 window.onload=function(){
   document.body.style.height = window.innerHeight + 'px';//修正某些界面下高度錯誤
+  /*
+  var act = function(){
+  	window.removeEventListener('devicemotion',act);
+  };
+  window.addEventListener('devicemotion',act,false);
+  */
+  
+  /*
+  //play and pause it once  for  ios
+  document.addEventListener('touchstart', function () {
+   document.getElementsByTagName('audio')[0].play();
+   document.getElementsByTagName('audio')[0].pause();
+  });
+  */
+  
 }
